97a76ce6fde0d08011d744dbea8bf76b054cc22d
Magnus Bergmark <magnus.bergmark@gmail.com>
2011-11-29 10:49:03 +0100

    Improve SporkInstance to handle spawning

--- lib/guard/spork/spork_instance.rb
+++ lib/guard/spork/spork_instance.rb
@@ -1,13 +1,36 @@
 module Guard
   class Spork
     class SporkInstance
-      attr_reader :port
+      attr_reader :env, :port, :pid
 
       def initialize(type, port, env, options)
         @type = type
         @port = port
         @env = env
         @options = options
+      end
+
+      def start
+        @pid = fork do
+          exec env, command
+        end
+      end
+
+      def stop
+        Process.kill('KILL', pid)
+      end
+
+      def alive?
+        return false unless pid
+        Process.waitpid(pid, Process::WNOHANG).nil?
+      end
+
+      def running?
+        return false unless pid
+        TCPSocket.new('localhost', port).close
+        true
+      rescue Errno::ECONNREFUSED
+        false
       end
 
       def command
@@ -23,6 +46,15 @@ module Guard
 
         parts << "-p #{port}"
         parts.join(" ")
+      end
+
+      def env_exec(environment, command)
+        if RUBY_VERSION > "1.9"
+          exec environment, command
+        else
+          environment.each_pair { |key, value| ENV[key] = value }
+          exec command
+        end
       end
 
       private
