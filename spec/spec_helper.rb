d8c69c885bb365ae4a857fd6b703d50bc486dd32
Cezary Baginski <cezary@chronomantic.net>
2014-12-12 01:59:36 +0100

    remove RSpec 3.1 deprecation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,4 @@ RSpec.configure do |config|
   config.color = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
